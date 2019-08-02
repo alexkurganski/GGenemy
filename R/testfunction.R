5e426761ce9604762a11ee3fbabc21c88e2b51e7
tajohu <tabea.huellen@gmx.de>
2019-08-02 22:40:38 +0200

    Add roxygen header to testfunction

--- R/testfunction.R
+++ R/testfunction.R
@@ -1,3 +1,12 @@
+#' Calculate the sum of all vector elements
+#'
+#' @param x vector 
+#'
+#' @return scalar
+#' @export
+#'
+#' @examples
+#' testfunction(c(1,2,3,4,5))
 testfunction <- function(x){
   sum(x)
 }
