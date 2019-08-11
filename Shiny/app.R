c92b1e9db60385d9f0076a1a3ff7445ef4ff5e80
Alex <alex.kurganski@stud.uni-goettingen.de>
2019-08-11 20:19:47 +0200

    Radiobox for var_name

--- Shiny/app.R
+++ Shiny/app.R
@@ -1,76 +1,85 @@
 source("shiny/conditional_densities.R")
-# Define UI for application that draws a histogram
+# UI
 ui <- shiny::navbarPage("Conditional Densities",
 
+    #First Tab - Reading Data
     shiny::tabPanel("Data",
+                    shiny::sidebarLayout(
+                        shiny::sidebarPanel(
+                            # Input: Read CSV-Data
+                            shiny::fileInput("file1", "Choose CSV File",
+                                             accept = c(
+                                                 "text/csv",
+                                                 "text/comma-separated-values,text/plain",
+                                                 ".csv")),
 
-    # Sidebar with a slider input for number of bins
-    shiny::sidebarLayout(
-        shiny::sidebarPanel(
-            shiny::fileInput("file1", "Choose CSV File",
-                             accept = c(
-                                 "text/csv",
-                                 "text/comma-separated-values,text/plain",
-                                 ".csv")),
-
-            # Input: Checkbox if file has header ----
-            shiny::checkboxInput("header", "Header", TRUE),
-
+                            # Input: Checkbox if file has header
+                            shiny::checkboxInput("header", "Header", TRUE),
 
 
-            # Input: Select separator ----
-            shiny::radioButtons("sep", "Separator",
-                                choices = c(Comma = ",",
-                                            Semicolon = ";",
-                                            Tab = "\t"),
-                                selected = ","),
 
-            # Input: Select quotes ----
-            shiny::radioButtons("quote", "Quote",
-                                choices = c(None = "",
-                                            "Double Quote" = '"',
-                                            "Single Quote" = "'"),
-                                selected = '"')
+                            # Input: Select separator
+                            shiny::radioButtons("sep", "Separator",
+                                                choices = c(Comma = ",",
+                                                Semicolon = ";",
+                                                Tab = "\t"),
+                                                selected = ","),
 
-            # Input: Select number of rows to display ----
-            # shiny::radioButtons("disp", "Display",
-            #                      choices = c(Head = "head",
-            #                                  All = "all"),
-            #                      selected = "head")
+                            # Input: Select quotes
+                            shiny::radioButtons("quote", "Quote",
+                                                choices = c(None = "",
+                                                            "Double Quote" = '"',
+                                                            "Single Quote" = "'"),
+                                                selected = '"')
 
-            ),
+                            # Not used, because we need full data
+                            # Input: Select number of rows to display ----
+                            # shiny::radioButtons("disp", "Display",
+                            #                      choices = c(Head = "head",
+                            #                                  All = "all"),
+                            #                      selected = "head")
+
+                            ),
         shiny::mainPanel(
+
+            # Output: Data
             shiny::tableOutput("contents")
             )
         )
-    ),
+        ),
 
     shiny::tabPanel("Plot",
+
                     shiny::sidebarLayout(
                         shiny::sidebarPanel(
+
+                            # Input Number Quantiles
                             shiny::sliderInput("quantiles",
                                                "Number of Quantiles:",
                                                min = 1,
                                                max = 10,
                                                value = 5),
-                            shiny::checkboxGroupInput("var_name",
-                                                      "",
-                                                      choices = NULL),
 
+                            # Select conditional variable
+                            shiny::radioButtons("var_name",
+                                                label = NULL,
+                                                choices = c("Dataset is missing")),
+
+                            # Select Variables to condition on
                             shiny::checkboxGroupInput("var_to_cond_on",
-                                                      "",
+                                                      label = NULL,
                                                       choices = NULL)
 
                             ),
-                 # Show a plot of the generated distribution
-                 shiny::mainPanel(
-                    lapply(1:7, function(i) {
-                        shiny::plotOutput(paste0('condplot', i))
-                    })
-                )
-            )
+                        # Show a plot of the generated distribution
+                        shiny::mainPanel(
+                            lapply(1:20, function(i) {
+                                shiny::plotOutput(paste0('condplot', i))
+                                })
+                            )
+                        )
+                    )
     )
-)
 
 server <- function(input, output, session){
     help <- list()
@@ -84,10 +93,10 @@ server <- function(input, output, session){
         df <- read.csv(inFile$datapath, header = input$header ,sep = input$sep,
                        quote = input$quote)
 
-        updateCheckboxGroupInput(session,
-                                 inputId = "var_name",
-                                 label = "condition variable",
-                                 choices = names(df))
+        updateRadioButtons(session,
+                           inputId = "var_name",
+                           label = "condition variable",
+                           choices = names(df))
 
         updateCheckboxGroupInput(session,
                                  inputId = "var_to_cond_on",
@@ -100,22 +109,25 @@ server <- function(input, output, session){
     output$contents <- shiny::renderTable({
         data()
     })
-        # input$file1 will be NULL initially. After the user selects
-        # and uploads a file, head of that data file by default,
-        # or all rows if selected, will be shown.
 
-    names <- c("V1","V2","V3","V4","V5","V6","V7")
+    #num_var <- reactive({1:length(input$var_to_cond_on)})
 
 
-    lapply(1:2,function(i) {
+    lapply(1:20,
+           function(i) {
     output[[paste0("condplot",i)]] <- shiny::renderPlot({
-        plot_single_conditional_density(data(),input$var_name,input$quantiles,input$var_to_cond_on[i])
+        plot_single_conditional_density(data(),
+                                        input$var_name,
+                                        input$quantiles,
+                                        input$var_to_cond_on[i])
         })
     })
 }
 
 # Run the application
 shiny::shinyApp(ui = ui, server = server)
-vari <- read.csv("variables.csv",row.names = "X")
-names(vari)
-?reactive()
+
+# to do:
+#Button für Anwendung
+#Fix categorical variables
+# Factors are not factors when red
