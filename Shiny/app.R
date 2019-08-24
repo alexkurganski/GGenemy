0d05534e13a82d50866fd69ffa9e9fab607c5afd
Alex <alex.kurganski@stud.uni-goettingen.de>
2019-08-24 14:04:03 +0200

    Minor bug fixes

--- Shiny/app.R
+++ Shiny/app.R
@@ -1,24 +1,24 @@
 source("shiny/conditional_densities.R")
 source("R/sum_stats.R")
-library(shinythemes)
+
 # UI
 ui <- shiny::navbarPage("Conditional Densities", theme = shinythemes::shinytheme("flatly"),
 
     #First Tab - Reading Data
-      shiny::tabPanel("Data",
+      shiny::tabPanel("Data-Upload",
                       shiny::sidebarLayout(
                             shiny::sidebarPanel(
                                 # Input: Read CSV-Data
-                                shiny::fileInput("file1", "Choose CSV File",
+                                shiny::fileInput("file1", "Choose txt/csv File",
                                                  accept = c(
-                                                     "text/csv",
+                                                     "txt/csv",
                                                      "text/comma-separated-values,text/plain",
                                                      ".csv")),
 
                                 # Input: Checkbox if file has header
-                                shiny::checkboxInput("header", "Header", TRUE),
-
-
+                                shiny::checkboxInput("header",
+                                                     label = "Header",
+                                                     TRUE),
 
                                 # Input: Select separator
                                 shiny::radioButtons("sep", "Separator",
@@ -32,42 +32,30 @@ ui <- shiny::navbarPage("Conditional Densities", theme = shinythemes::shinytheme
                                                     choices = c(None = "",
                                                                 "Double Quote" = '"',
                                                                 "Single Quote" = "'"),
-                                                    selected = '"')
-
-                            
-                                # Not used, because we need full data
-                                # Input: Select number of rows to display ----
-                                # shiny::radioButtons("disp", "Display",
-                                #                      choices = c(Head = "head",
-                                #                                  All = "all"),
-                                #                      selected = "head")
-
+                                                    selected = '"'),
+                                
+                                shiny::checkboxInput("rownames",
+                                                     label = "First row as rownames",
+                                                     FALSE)
+                                
                                 ),
-            shiny::mainPanel(
+                            
+                            shiny::mainPanel(
+                              shiny::tableOutput("contents")
+                              )
+                            )
+                      ),
+#############################################################################
 
-                # Output: Data
-                shiny::tableOutput("contents")
-                )
-            )
-        ),
-####################################################################################
-    ##### CHOOSING FACTOR DOES NOT WORK PROPERLY ###########
-###########################################################
-
-    shiny::tabPanel("Summary/Structure",
+    shiny::tabPanel("Data-Structure",
                     
                     shiny::sidebarLayout(
                         shiny::sidebarPanel(
                             
                             # Input Number Quantiles
                             shiny::checkboxGroupInput("as.factor",
-                                                      label = NULL,
-                                                      choices = NULL),
-                            
-                            shiny::actionButton(inputId = "clicks2",
-                                                label = "Calculate!")
-                            
-
+                                                      label = "",
+                                                      choices = NULL)
                         ),
                         
         shiny::mainPanel(
@@ -77,6 +65,7 @@ ui <- shiny::navbarPage("Conditional Densities", theme = shinythemes::shinytheme
             ),
         )
         ),
+
 #####################################################################################    
     shiny::tabPanel("Summary_Stats",
                 
@@ -84,7 +73,7 @@ ui <- shiny::navbarPage("Conditional Densities", theme = shinythemes::shinytheme
                         shiny::sidebarPanel(
                         
                             shiny::radioButtons("var_name",
-                                                label = NULL,
+                                                label = "",
                                                 choices = c("Dataset is missing")),
                             
                             shiny::sliderInput("quantiles_sum_stats",
@@ -122,12 +111,12 @@ ui <- shiny::navbarPage("Conditional Densities", theme = shinythemes::shinytheme
 
                             # Select conditional variable
                             shiny::radioButtons("var_name2",
-                                                label = NULL,
+                                                label = "",
                                                 choices = c("Dataset is missing")),
 
                             # Select Variables to condition on
                             shiny::checkboxGroupInput("var_to_cond_on",
-                                                      label = NULL,
+                                                      label = "",
                                                       choices = NULL),
                             
                             
@@ -150,110 +139,121 @@ ui <- shiny::navbarPage("Conditional Densities", theme = shinythemes::shinytheme
 #################################################################################
 # server
 server <- function(input, output, session){
+  
+  data1 <- shiny::reactive({
     
-    varnames <- reactiveValues()
+    shiny::req(input$file1) # require that the input is available
+    
+    inFile <- input$file1
+    
+    df <- read.csv(inFile$datapath,
+                   header = input$header,
+                   sep = input$sep,
+                   quote = input$quote,
+                   row.names = if(input$rownames){1} else {NULL}
+                   )
+    
+    shiny::updateCheckboxGroupInput(session,
+                                    inputId = "as.factor",
+                                    label = "Which Variable is a factor?",
+                                    choices = names(df))
+            
+    shiny::updateRadioButtons(session,
+                              inputId = "var_name",
+                              label = "Condition variable",
+                              choices = names(df))
+            
+    shiny::updateRadioButtons(session,  
+                              inputId = "var_name2",
+                              label = "Condition variable",
+                              choices = names(df))
+    
+    shiny::updateCheckboxGroupInput(session,
+                                    inputId = "var_to_cond_on",
+                                    label = "Variables to condition on",
+                                    choices = names(df))
+    return(df)
+    })
+  
+  data2 <- shiny::reactive({
+    
+    shiny::req(input$file1) # require that the input is available
+    
+    inFile <- input$file1
+    df <- read.csv(inFile$datapath, 
+                   header = input$header,
+                   sep = input$sep,
+                   quote = input$quote,
+                   row.names = if(input$rownames){1} else {NULL})
+    
+    for(i in unlist(input$as.factor,use.names = FALSE)){
+      df[,i] <- as.factor(df[,i])
+      }
+    df_reduced <- na.omit(df)
+    return(df_reduced)
+    })
+  
+####################################################################  
+# Data-Upload Tab        
 
-        data1 <- shiny::reactive({
-            shiny::req(input$file1) # require that the input is available
+  output$contents <- shiny::renderTable({
+    head(data1())
+    })
         
-            isolate(inFile <- input$file1)
-            
-            isolate(df <- read.csv(inFile$datapath, header = input$header, sep = input$sep,
-                           quote = input$quote))
-            
-            #eventReactive(input$clicks2, {
-            #varnames$names <- names(df)})
-            
-            shiny::updateCheckboxGroupInput(session,
-                                            inputId = "as.factor",
-                                            label = "factor variable",
-                                            choices = names(df))
-            
-            shiny::updateRadioButtons(session,
-                                      inputId = "var_name",
-                                      label = "condition variable",
-                                      choices = names(df))
-            
-            shiny::updateRadioButtons(session,
-                                      inputId = "var_name2",
-                                      label = "condition variable",
-                                      choices = names(df))
-    
-            shiny::updateCheckboxGroupInput(session,
-                                     inputId = "var_to_cond_on",
-                                     label = "Variables to condition on",
-                                     choices = names(df))
-            
-            #df[,input$as.factor] <- as.factor(df[,input$as.factor])
-            
-          return(df)
-        })
+  output$na1 <- shiny::renderPrint({
+    nrow(data1())
+    })
+        
+  output$na2 <- shiny::renderPrint({
+    nrow(data2())
+    })
+  
+####################################################################        
+# Data-Management Tab
 
-        data2 <- shiny::reactive({
-            shiny::req(input$file1) # require that the input is available
-            
-            isolate(inFile <- input$file1)
-            
-            isolate(df <- read.csv(inFile$datapath, header = input$header, sep = input$sep,
-                                   quote = input$quote))
-            
-            for(i in unlist(input$as.factor,use.names = FALSE)){
-            df[,i] <- as.factor(df[,i])
-            }
-            
-            return(df)
-        })
-     #   observe({
-     #       shiny::req(input$file1)
-     #   shiny::updateCheckboxGroupInput(session,
-     #                                   inputId = "as.factor",
-     #                                   label = "factor variable",
-     #                                   choices = names(data()$results))
-     #   })
-        
-        #observeEvent(input$clicks2,
-        #    data1() <- as.factor(data1$"V6")
-        #)
-        
-        output$contents <- shiny::renderTable({
-            head(data1())
-        })
-        output$summary1 <- shiny::renderPrint({
-            summary(data2())
-        })
+  output$summary1 <- shiny::renderPrint({
+    summary(data2())
+    })
     
-        output$str1 <- shiny::renderPrint({
-            str(data2())
-        })
+  output$str1 <- shiny::renderPrint({
+    str(data2())
+    })
     
-        output$sum_stats1 <- shiny::renderPrint({
-            sum_stats(input$var_name,
-                      data2(),
-                      input$quantiles_sum_stats,
-                      input$n_sum_stats
-                      )
-        })
-    
-        shiny::observeEvent(input$clicks, {
-            len <- length(as.numeric(input$var_to_cond_on))
-            lapply(1:len,
-                   function(i) {
-                       output[[paste0("condplot",i)]] <- shiny::renderPlot({
-                           plot_single_conditional_density(data2(),
-                                                           input$var_name2,
-                                                           input$quantiles,
-                                                           isolate(input$var_to_cond_on[i])
-                                                           )
-                    })
-                })
-        },
-        ignoreInit = TRUE)
+####################################################################
+#Sum-stats Tab  
+  
+  output$sum_stats1 <- shiny::renderPrint({
+    sum_stats(input$var_name,
+              data2(),
+              input$quantiles_sum_stats,
+              input$n_sum_stats
+              )
+    })
+#####################################################################
+#Cond Plot Densities
 
-}
+  shiny::observeEvent(input$clicks, {
+    len <- length(as.numeric(input$var_to_cond_on))
+    lapply(1:len,function(i) {
+      output[[paste0("condplot",i)]] <- shiny::renderPlot({
+        plot_single_conditional_density(data2(),
+                                        input$var_name2,
+                                        input$quantiles,
+                                        isolate(input$var_to_cond_on[i])
+                                        )
+        })
+      })
+    },
+    ignoreInit = TRUE)
+  }
 
 # Run the application
 shiny::shinyApp(ui = ui, server = server)
+
+#########################################################################################
 # to do:
+#########################################################################################
+
 # 1.!!!!!!
 # Fix categorical variables - can not fix it - Help Tabea:
 # Eather I try it in the df part - reloades Input without values
@@ -273,10 +273,21 @@ shiny::shinyApp(ui = ui, server = server)
 # 4.
 # The 4th argument of sum stats only works properly.
 # The function has big problems with factorial variables. Did not look into it,
-# why it did so much problems
+# why it is not working properly
 
 # 5.
-# Better looking outputs for summary/str and sum stats
+# Better looking outputs for Data-Upload, Data-Management and sum stats
 # Furthermore choose theme
 
 # 6. Stop warnings
+
+# 7. What to do with NA's?
+
+# 8. Spacing relevent? Example: Dataset with many variables won't fit with head().
+
+# 9. obtain Code function?
+
+# 10. Which Data do we allow to read into our dataset? We have now csv and txt.
+# RDs?
+
+# 11. CheckboxInput does not show the label. Bug..
