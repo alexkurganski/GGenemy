7cc0caa381876b94bad2e5a3ec416fe15676a3cd
tajohu <tabea.huellen@gmx.de>
2019-09-02 22:34:43 +0200

    Function to run the Shiny App

---
+++ R/GGenemy.R
@@ -0,0 +1,13 @@
+#' GGenemy
+#' 
+#' Starts the Shiny App in which a dataset can be reviewed, several summary statistics can be 
+#' calculated and conditional plots can be drawn.
+#'
+#' @return A Shiny App.
+#' @export
+#'
+#' @examples
+#' # coming soon.
+GGenemy <- function(){
+  shiny::runApp("./Shiny")
+}
