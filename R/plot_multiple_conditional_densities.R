25c795db68dce26c0e2848bcfe1c4291f56c2186
tajohu <tabea.huellen@gmx.de>
2019-09-07 10:00:09 +0200

    Rename parameters, fix legends for given factors

--- R/plot_multiple_conditional_densities.R
+++ R/plot_multiple_conditional_densities.R
@@ -7,9 +7,9 @@
 #'
 #'
 #' @param dataset A dataframe.
-#' @param var_name A variable from your \code{dataset}.
-#' @param n_quantiles Number of quantiles you want to partition \code{var_name} into.
-#' @param var_to_cond_on Vector of Variables to condition on from your \code{dataset}.
+#' @param given_var A variable from your \code{dataset} which you want to set as a condition.
+#' @param n_quantiles Number of quantiles you want to partition \code{given_var} into.
+#' @param var_to_plot Vector of variables from your \code{dataset} which you want to plot.
 #'
 #' @return Several ggplot graphics. The total number of plots will equal the
 #'   number of variables in your \code{dataset}.
@@ -34,17 +34,17 @@
 
 # 1.Calculate the quantiles for selected variable 
 # 2.Plot densities of chosen other variables based on the calculated quantiles
-plot_multiple_conditional_densities <- function(dataset, var_name, n_quantiles = 5, var_to_cond_on) {
+plot_multiple_conditional_densities <- function(dataset, given_var, n_quantiles = 5, var_to_plot) {
   
-  # 1.1 categorical var_name
-  if (is.factor(dataset[, var_name])) {
-    if (n_quantiles != length(levels(dataset[, var_name]))) {
-      warning(paste0(var_name, " is a categorical variable. The number of categories will be defined as a condition."))
+  # 1.1 categorical given_var
+  if (is.factor(dataset[, given_var])) {
+    if (n_quantiles != length(levels(dataset[, given_var]))) {
+      warning(paste0(given_var, " is a categorical variable. The number of categories will be defined as a condition."))
     }
     data_help <- dataset
-    data_help$quant <- as.factor(as.numeric(dataset[, var_name]))
-  } else { # 1.2 continuous var_name
-    var_goal <- dplyr::select(dataset, var_name)[, 1]
+    data_help$quant <- dataset[, given_var]
+  } else { # 1.2 continuous given_var
+    var_goal <- dplyr::select(dataset, given_var)[, 1]
     quantiles <- stats::quantile(var_goal, 1:(n_quantiles - 1) / (n_quantiles))
     quantiles <- as.numeric(quantiles)
     data_help <- dataset
@@ -55,16 +55,16 @@ plot_multiple_conditional_densities <- function(dataset, var_name, n_quantiles =
   # 2.
   names_var <- names(dataset)
   g <- ggplot2::ggplot(data_help, ggplot2::aes(fill = quant)) + ggplot2::theme_minimal()
-  lapply(var_to_cond_on, function(a) {
-    if (is.factor(data_help[, a]) & is.factor(data_help[, var_name])) {
-      g + ggplot2::geom_bar(ggplot2::aes_string(x = a, fill = var_name)) +
-        ggplot2::ggtitle(paste0(a, " conditional on ", var_name))
+  lapply(var_to_plot, function(a) {
+    if (is.factor(data_help[, a]) & is.factor(data_help[, given_var])) {
+      g + ggplot2::geom_bar(ggplot2::aes_string(x = a, fill = given_var)) +
+        ggplot2::ggtitle(paste0(a, " conditional on ", given_var))
     } else if (is.factor(data_help[, a])) {
-      g + ggplot2::geom_boxplot(ggplot2::aes_string(x = a, y = var_name)) +
-        ggplot2::ggtitle(paste0(a, " conditional on ", var_name))
+      g + ggplot2::geom_boxplot(ggplot2::aes_string(x = a, y = given_var)) +
+        ggplot2::ggtitle(paste0(a, " conditional on ", given_var))
     } else {
       g + ggplot2::geom_density(alpha = 0.5) + ggplot2::aes_string(x = a) +
-        ggplot2::ggtitle(paste0(a, " conditional on ", var_name))
+        ggplot2::ggtitle(paste0(a, " conditional on ", given_var))
     }
   })
 }
--- R/plot_single_conditional_density.R
+++ R/plot_single_conditional_density.R
@@ -11,9 +11,9 @@
 #' depending on whether the conditional variable is continuous or categorical.
 #'
 #' @param dataset A dataframe.
-#' @param var_name The variable of interest from your \code{dataset}.
-#' @param n_quantiles Number of quantiles you want to partition \code{var_name} into.
-#' @param var_to_cond_on A variable to condition on from your \code{dataset}.
+#' @param given_var A variable from your \code{dataset} which you want to set as a condition.
+#' @param n_quantiles Number of quantiles you want to partition \code{given_var} into.
+#' @param var_to_plot A variable to condition on from your \code{dataset}.
 #'
 #' @return A ggplot graphic.
 #' @export
@@ -38,22 +38,22 @@
 # 1.Calculate the quantiles for selected variable
 # 2.Plot densities of one variable based on the calculated quantiles
 
-plot_single_conditional_density <- function(dataset, var_name, n_quantiles = 5, var_to_cond_on) {
+plot_single_conditional_density <- function(dataset, given_var, n_quantiles = 5, var_to_plot) {
 
-  # 1.1 categorial var_name
-  if (is.factor(dataset[, var_name])) {
-    if (n_quantiles != length(levels(dataset[, var_name]))) {
-      warning(paste0(var_name, " is a categorical variable. The number of categories will be defined as a condition."))
+  # 1.1 categorial given_var
+  if (is.factor(dataset[, given_var])) {
+    if (n_quantiles != length(levels(dataset[, given_var]))) {
+      warning(paste0(given_var, " is a categorical variable. The number of categories will be defined as a condition."))
     }
     data_help <- dataset
-    data_help$quant <- as.factor(as.numeric(dataset[, var_name]))
-  } else { # 1.2 continuous var_name
+    data_help$quant <- dataset[, given_var]
+  } else { # 1.2 continuous given_var
     if (n_quantiles == 1) {
       data_help <- dataset
       data_help$quant <- 1
       data_help$quant <- as.factor(data_help$quant)
     } else {
-      var_goal <- dplyr::select(dataset, var_name)[, 1]
+      var_goal <- dplyr::select(dataset, given_var)[, 1]
       quantiles <- stats::quantile(var_goal, 1:(n_quantiles - 1) / (n_quantiles))
       quantiles <- as.numeric(quantiles)
       data_help <- dataset
@@ -65,14 +65,14 @@ plot_single_conditional_density <- function(dataset, var_name, n_quantiles = 5, 
   # 2.
   g <- ggplot2::ggplot(data_help, ggplot2::aes(fill = quant)) + ggplot2::theme_minimal()
 
-  if (is.factor(data_help[, var_to_cond_on]) & is.factor(data_help[, var_name])) {
-    g + ggplot2::geom_bar(ggplot2::aes_string(x = var_to_cond_on, fill = var_name)) +
-      ggplot2::ggtitle(paste0(var_to_cond_on, " conditional on ", var_name))
-  } else if (is.factor(data_help[, var_to_cond_on])) {
-    g + ggplot2::geom_boxplot(ggplot2::aes_string(x = var_to_cond_on, y = var_name)) +
-      ggplot2::ggtitle(paste0(var_to_cond_on, " conditional on ", var_name))
+  if (is.factor(data_help[, var_to_plot]) & is.factor(data_help[, given_var])) {
+    g + ggplot2::geom_bar(ggplot2::aes_string(x = var_to_plot, fill = given_var)) +
+      ggplot2::ggtitle(paste0(var_to_plot, " conditional on ", given_var))
+  } else if (is.factor(data_help[, var_to_plot])) {
+    g + ggplot2::geom_boxplot(ggplot2::aes_string(x = var_to_plot, y = given_var)) +
+      ggplot2::ggtitle(paste0(var_to_plot, " conditional on ", given_var))
   } else {
-    g + ggplot2::geom_density(alpha = 0.5) + ggplot2::aes_string(x = var_to_cond_on) +
-      ggplot2::ggtitle(paste0(var_to_cond_on, " conditional on ", var_name))
+    g + ggplot2::geom_density(alpha = 0.5) + ggplot2::aes_string(x = var_to_plot) +
+      ggplot2::ggtitle(paste0(var_to_plot, " conditional on ", given_var))
   }
 }
