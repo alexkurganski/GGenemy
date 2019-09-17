704dbdf83f627908356355cbd90733a9e4637fad
tajohu <tabea.huellen@gmx.de>
2019-09-17 16:27:34 +0200

    Move helper functions

--- R/helpers.R
+++ R/helpers.R
@@ -33,6 +33,45 @@ quantile0.75 <- function(data, na.rm = TRUE) {
   return(out)
 }
 
+
+# Helper function to read in data frames from the global environment
+
+#' Internal: Seatch Dataframe in GE
+#'
+#' Searches for Dataframes in the Global Environment.
+#' @keywords internal
+
+search_dataframe <- function() {
+  # Get components of Global Environment
+  namesGE <- ls(envir = .GlobalEnv)
+  
+  # Look for bamlss & gamlss components
+  dataframe_true <- sapply(namesGE,check_dataframe)
+  
+  # Return nothing if no bamlss or gamlss
+  if (length(dataframe_true) > 0)
+    if (sum(dataframe_true) > 0)
+      return(namesGE[dataframe_true])
+  else
+    return("")
+}
+
+#' Internal: Dataframe Checker
+#'
+#' Check whether the insertet object is a dataframe or not.
+#' @keywords internal
+check_dataframe <- function(x) {
+  if (is.character(x))
+    obj <- get(x, envir = .GlobalEnv)
+  else
+    obj <- x
+  if (is.data.frame(obj))
+    return(TRUE)
+  else
+    return(FALSE)
+}
+
+
 # Helper function to load ggplot objects into the global environment
 
 assign_to_global <- function(input, value, pos=1){
