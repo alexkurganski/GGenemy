dfa104200c069821cb5aa5501f5e64f9db1b3882
tajohu <tabea.huellen@gmx.de>
2019-09-17 16:28:12 +0200

    New tests for describe

--- tests/testthat/test_describe.R
+++ tests/testthat/test_describe.R
@@ -1,9 +1,3 @@
-#test_that("describe needs a valid input", {
-  #expect_error(describe( ,num.desc = c("mean", "median", "var", "sd", "valid.n"),
-                        #xname = NA),
-               #"Usage: describe(x,...) where x is a vector, data frame or matrix")
-#})
-
 test_that("describe automatically creates varnames if none are given", {
   data(iris)
   names(iris) <- NULL
@@ -17,16 +11,54 @@ test_that("describe correctly separates numerical variables and factors", {
   desc <- describe(iris)
   
   for(i in 1:length(desc[[1]])){
-    expect_true(is.numeric(iris[, names(desc[[1]][i])]))
+    expect_true(is.numeric(iris[, names(desc$Numeric[i])]))
   }
   
   for(i in 1:length(desc[[2]])){
-    expect_true(is.factor(iris[, names(desc[[2]][i])]))
+    expect_true(is.factor(iris[, names(desc$Factor[i])]))
   }
   
 })
 
 
-#test_that("describe calculates the correct summary statistics", {
+test_that("describe calculates the correct summary statistics for numeric
+          variables", {
+  data(iris)
+  desc <- describe(iris)
   
-#})
+  for(i in 1:length(desc[[1]])){
+    expect_equal(desc[[1]][[i]][[1]], mean(iris[, i]))
+  }
+
+  for(i in 1:length(desc[[1]])){
+    expect_equal(desc[[1]][[i]][[2]], median(iris[, i]))
+  }
+  
+  for(i in 1:length(desc[[1]])){
+    expect_equal(desc[[1]][[i]][[3]], var(iris[, i]))
+  }
+
+  for(i in 1:length(desc[[1]])){
+    expect_equal(desc[[1]][[i]][[4]], sd(iris[, i]))
+  }
+
+  for(i in 1:length(desc[[1]])){
+    expect_equal(desc[[1]][[i]][[5]], valid.n(iris[, i]))
+  }
+  
+})
+
+test_that("describe calculates the correct statistics for factors", {
+  data(iris)
+  desc <- describe(iris)
+  ind1 <- c(1,3,5)
+  ind2 <- c(2,4,6)
+  
+  for(i in ind1){
+    expect_equal(desc[[2]][[1]][[i]], 50)
+  }
+  
+  for(i in ind2){
+    expect_equal(desc[[2]][[1]][[i]], (50/150)*100)
+  }
+})
