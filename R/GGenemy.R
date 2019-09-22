57f317b1a55a597f9aa5639b684efb3b45c73e3a
tajohu <tabea.huellen@gmx.de>
2019-09-22 18:04:51 +0200

    JS function as string

--- R/GGenemy.R
+++ R/GGenemy.R
@@ -12,26 +12,26 @@
 
 GGenemy <- function() {
   
-  jsCode <- "shinyjs.init = function(){
-    $('#GGenemy li a[data-value='2. Data Structure']').hide();
-    $('#GGenemy li a[data-value='3. Summary Statistics']').hide();
-    $('#GGenemy li a[data-value='4. Plots']').hide();
-    $('#file1').parent().parent().parent().parent().hide();
-    $('#header').hide();
-    $('#sep').hide();
-    $('#quote').hide();
-    $('#decimals').hide();
-    $('#rownames').hide();
-    $('#border1').hide();
-    $('#border2').hide();
-    $('#border3').hide();
-    $('#border4').hide();
-    $('#border5').hide();
-  }"
+  #jsCode <- "shinyjs.init = function(){
+    #$('#GGenemy li a[data-value='2. Data Structure']').hide();
+    #$('#GGenemy li a[data-value='3. Summary Statistics']').hide();
+    #$('#GGenemy li a[data-value='4. Plots']').hide();
+    #$('#file1').parent().parent().parent().parent().hide();
+    #$('#header').hide();
+    #$('#sep').hide();
+    #$('#quote').hide();
+    #$('#decimals').hide();
+    #$('#rownames').hide();
+    #$('#border1').hide();
+    #$('#border2').hide();
+    #$('#border3').hide();
+    #$('#border4').hide();
+    #$('#border5').hide();
+  #}"
   
   ui <- fluidPage(
     shinyjs::useShinyjs(),
-    shinyjs::extendShinyjs(text = jsCode),
+    #shinyjs::extendShinyjs(text = jsCode),
     
     # CSS defaults #############################################################
     tags$style(type = 'text/css', 
